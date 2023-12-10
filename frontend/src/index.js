4437947ca83b032e178b83c4f834983ade7e5c59
Rômulo Galvão Machado <52431713+rgalvaomachado@users.noreply.github.com>
2023-12-10 16:29:08 -0300

    primeiro commit

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App.js';
+
+
+// JSX: html dentro do JS (Javascropt XML)
+
+render(<App></App>,document.getElementById('app'));
---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+// Babel: converter (transpilar) codigo do rect paara que o brownser entenda
+// Webpack: Para cda tipo de arquivo (js, css, png) eu vou converter o codigo de uma maneira diferente
+//          Loaders: babel-loaders, css-loader, image-loader
+
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+};
